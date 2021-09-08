a58105a85f48b41a8052e3ccdf039a926f53cb9c
Wes Richter <s1984425@7-11.com>
2021-09-07 23:49:31 -0500

    Updated package.json to include new custom form Validator * inputter

--- success/src/containers/SuccessContainer/SuccessContainer.tsx
+++ success/src/containers/SuccessContainer/SuccessContainer.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-//import builtImg from '../../assets/built.svg'
+import builtImg from '../../assets/built.svg'
 import './styles.css'
 
 export default class SuccessContainer extends Component {
@@ -8,6 +8,7 @@ export default class SuccessContainer extends Component {
         const successName: string = localStorage.getItem('formFirstName')
         return (
             <div data-testid='test-1' className="mainContainer">
+                <img src={builtImg} alt="built main" />
                 <h3>Congrats!</h3>
                 <p>{successName}, you have submitted the form successfully.</p>
                 <a href="/loan-application">Submit Another Application</a>
